4a773a95dcde0555adb2cf2b32c64450c9935969
ydkulks <ydkulks2@gmail.com>
2020-12-31 21:39:03 +0530

    Login page

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,25 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
+user = {'username': 'Ydkulks'}
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Perchè'}
     posts = [
         {
-            'author': {'username': 'Lorem'},
-            'body': 'Cos’è Lorem Ipsum? Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa.'
+            'author': {'username': 'Suresh'},
+            'body': 'Do anybody know how to start learning flask?'
         },
         {
-            'author': {'username': 'Ipsum'},
-            'body': 'Cos’è Lorem Ipsum? Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa.'
+            'author': {'username': 'Inchara'},
+            'body': 'Finished watching Peaky Blinders yesterday,its so awesome .'
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign in', form=form)
